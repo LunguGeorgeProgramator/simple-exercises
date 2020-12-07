62dcfdec49ee80809b6440f04f06f49543c21004
LunguGeorgeProgramator <51510106+LunguGeorgeProgramator@users.noreply.github.com>
2020-12-07 20:38:53 +0200

    add print out as comment in script

--- sort.php
+++ sort.php
@@ -5,7 +5,7 @@
 $array_to_sort = [50, 345, 6, 345, 1, 4, 100, 2, 503, 3, 1, 345];
 $array_sorted  = [];
 
-echo 'Inainte de sortare ' . var_dump($array_to_sort) . ' </br>';
+echo 'Before sort ' . var_dump($array_to_sort) . ' </br>';
 
 while( true ){ /// loop the code until the break is encountered 
     // when the array to be sorted is empty all sorted out break
@@ -31,7 +31,40 @@ while( true ){ /// loop the code until the break is encountered
         break;
     }
 
+
+
 }
 
 
-echo 'Dupa sortare ' . var_dump($array_sorted) . ' </br>';
+echo 'After sort ' . var_dump($array_sorted) . ' </br>';
+
+    /**
+     * C:\wamp64\www\sort\index.php:6:
+     * Before sort:
+     * array (size=12)
+     * 0 => int 50
+     * 1 => int 345
+     * 2 => int 6
+     * 3 => int 345
+     * 4 => int 1
+     * 5 => int 4
+     * 6 => int 100
+     * 7 => int 2
+     * 8 => int 503
+     * 9 => int 3
+     * 10 => int 1
+     * 11 => int 345
+     * 
+     * C:\wamp64\www\sort\index.php:35:
+     * After sort
+     * array (size=9)
+     * 1 => int 1
+     * 2 => int 2
+     * 3 => int 3
+     * 4 => int 4
+     * 5 => int 6
+     * 6 => int 50
+     * 7 => int 100
+     * 10 => int 345
+     * 11 => int 503
+     */
